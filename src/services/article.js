2f6793d2ace1d1ccb3038fc8d0f0bbeb90ee449a
Phiroj Shah <41940520+phirojshah@users.noreply.github.com>
2023-07-10 19:14:28 +0545

    Update article.js

--- src/services/article.js
+++ src/services/article.js
@@ -1,7 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
-console.log(rapidApiKey);
 
 export const articleApi = createApi({
   reducerPath: "articleApi",
